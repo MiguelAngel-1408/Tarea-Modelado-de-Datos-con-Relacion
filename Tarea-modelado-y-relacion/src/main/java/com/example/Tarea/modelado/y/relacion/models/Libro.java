e8147e03be2fee07e7f208f93d21352a1e5fc9a5
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-02-22 18:47:08 -0500

    feat: ClaseLibro completada

---
+++ Tarea-modelado-y-relacion/src/main/java/com/example/Tarea/modelado/y/relacion/models/Libro.java
@@ -0,0 +1,44 @@
+package com.example.Tarea.modelado.y.relacion.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "libros")
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String titulo;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id", nullable = false)
+    private Autor autor;
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }  
+}
