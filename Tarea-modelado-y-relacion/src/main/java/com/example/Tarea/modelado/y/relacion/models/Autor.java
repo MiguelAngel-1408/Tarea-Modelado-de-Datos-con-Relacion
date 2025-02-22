11bd2c2400ebbd3c74794606a54a6fb842c41ed1
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-02-22 18:45:13 -0500

    feat: ClaseAutor completada

---
+++ Tarea-modelado-y-relacion/src/main/java/com/example/Tarea/modelado/y/relacion/models/Autor.java
@@ -0,0 +1,43 @@
+package com.example.Tarea.modelado.y.relacion.models;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Libro> libros;
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(List<Libro> libros) {
+        this.libros = libros;
+    }
+}
