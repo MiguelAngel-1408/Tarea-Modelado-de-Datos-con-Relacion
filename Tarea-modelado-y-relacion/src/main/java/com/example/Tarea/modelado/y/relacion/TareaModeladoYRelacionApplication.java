8b5ab0bd008665ded62943c98f415075f70e514f
MiguelAngel-1408 <patino.miguel.0232@eam.edu.co>
2025-02-22 18:38:54 -0500

    feat: configuracion SQL agregada

---
+++ Tarea-modelado-y-relacion/src/main/java/com/example/Tarea/modelado/y/relacion/TareaModeladoYRelacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tarea.modelado.y.relacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaModeladoYRelacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaModeladoYRelacionApplication.class, args);
+	}
+
+}
---
+++ Tarea-modelado-y-relacion/src/test/java/com/example/Tarea/modelado/y/relacion/TareaModeladoYRelacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tarea.modelado.y.relacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaModeladoYRelacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
